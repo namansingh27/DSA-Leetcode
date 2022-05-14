c1eb84cb1308c8b772e551f22aa850a6b3268504
Naman Singh <87642087+namansingh27@users.noreply.github.com>
2022-05-15 00:26:51 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (18.19%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -1,33 +1,36 @@
-public class Solution {
-    public boolean isValidBST(TreeNode root) {
-        if(root == null)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+   long prev=Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root){
+        if(root==null)
             return true;
- 
-        LinkedList<BNode> queue = new LinkedList<BNode>();
-        queue.add(new BNode(root, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY));
-        while(!queue.isEmpty()){
-            BNode b = queue.poll();
-            if(b.n.val <= b.left || b.n.val >=b.right){
-                return false;
-            }
-            if(b.n.left!=null){
-                queue.offer(new BNode(b.n.left, b.left, b.n.val));
-            }
-            if(b.n.right!=null){
-                queue.offer(new BNode(b.n.right, b.n.val, b.right));
-            }
-        }
+        
+        boolean la =isValidBST(root.left);
+        if(la==false)
+            return false;
+        
+        if(prev>=root.val)
+            return false;
+        prev=root.val;
+        
+        boolean ra= isValidBST(root.right);
+        if(ra==false)
+            return false;
+        
         return true;
-    }
-}
-//define a BNode class with TreeNode and it's boundaries
-class BNode{
-    TreeNode n;
-    double left;
-    double right;
-    public BNode(TreeNode n, double left, double right){
-        this.n = n;
-        this.left = left;
-        this.right = right;
     }
 }
